78010e2d53076bbb0067944e2121e8d3f39dda2c
seunghunbang <upi21x@gmail.com>
2023-01-28 03:49:06 +0200

    git ignore updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
-      <div className="flex"></div>
+      <div className="flex justify-center items-center text-center">
+        This is Home
+      </div>
     </div>
   );
 }
