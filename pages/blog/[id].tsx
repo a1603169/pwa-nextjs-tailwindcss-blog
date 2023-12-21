17202fc6c01571f47b07b6d7edfdb7e202285e67
seunghunbang <upi21x@gmail.com>
2023-12-22 02:49:25 +0900

    Tags are added

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -7,26 +7,42 @@ export default function Post({
   postData: {
     title: string;
     date: string;
+    tags: string[];
     contentHtml: string;
   };
 }) {
   return (
     <>
-    <Head>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-vsc-dark-plus.min.css" rel="stylesheet" />
+      <Head>
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-vsc-dark-plus.min.css"
+          rel="stylesheet"
+        />
         <title>{postData.title}</title>
         <meta name="description" content={postData.title} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <article className="flex flex-col items-center justify-center text-xl gap-5 pb-14 max-w-700 w-full my-0 mx-auto px-2">
-      <div className="flex flex-col w-full items-start">
-        <p className="whitespace-nowrap">{postData.date}</p>
-        <h1 className="text-3xl w-full">{postData.title}</h1>
-      </div>
-      <hr className="text-indigo-500 w-full"></hr>
-      <div className="w-full" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </article>
+      <article className="flex flex-col items-center justify-center text-xl gap-5 pb-14 max-w-700 w-full my-0 mx-auto px-2">
+        <div className="flex flex-col w-full items-start">
+          <p className="whitespace-nowrap">{postData.date}</p>
+          <h1 className="text-3xl w-full">{postData.title}</h1>
+          <ul className="flex gap-2">
+            {postData.tags.map((tag, idx) => {
+              return (
+                <li key={idx} className="whitespace-nowrap bg-slate-400  text-slate-50 py-0.5 px-2 rounded-lg">
+                  {tag}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <hr className="text-indigo-500 w-full"></hr>
+        <div
+          className="w-full"
+          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+        />
+      </article>
     </>
   );
 }
