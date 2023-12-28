f68731c5b5b700cc47a0eccc0e77c5ac1d0ded54
seunghunbang <upi21x@gmail.com>
2023-12-28 14:14:23 +0900

    Update Filtering with clearing funcs

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 type FiltersProps = {
   tags: Object;
@@ -6,31 +6,48 @@ type FiltersProps = {
   setSelectedTags: (tags: string[]) => void;
 };
 
-export default function Filters({ tags, selectedTags, setSelectedTags }: FiltersProps) {
+export default function Filters({
+  tags,
+  selectedTags,
+  setSelectedTags,
+}: FiltersProps) {
   const handleTagClick = (tag: string) => {
     if (selectedTags.includes(tag)) {
-      setSelectedTags(selectedTags.filter((selectedTag) => selectedTag !== tag));
+      setSelectedTags(
+        selectedTags.filter((selectedTag) => selectedTag !== tag)
+      );
     } else {
       setSelectedTags([...selectedTags, tag]);
     }
   };
 
   return (
-    <div className="w-3/4 duration-300 flex gap-2 mb-2 flex-wrap justify-center cursor-pointer max-md:text-lg">
-      {Object.entries(tags).map(([key, value], idx) => {
-        const isSelected = selectedTags.includes(key);
-        return (
-          <div
-            key={idx}
-            className={`flex grow items-center justify-center gap-2 border-2 border-solid p-2 rounded-lg ${
-              isSelected ? 'bg-indigo-300 text-white' : ''
-            }`}
-            onClick={() => handleTagClick(key)}
-          >
-            {key}({value})
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="w-3/4 duration-300 flex gap-2 mb-2 flex-wrap justify-center cursor-pointer max-md:text-lg">
+        {Object.entries(tags).map(([key, value], idx) => {
+          const isSelected = selectedTags.includes(key);
+          return (
+            <div
+              key={idx}
+              className={`flex grow items-center justify-center gap-2 border-2 border-solid p-2 rounded-lg ${
+                isSelected ? "bg-indigo-300 text-white" : ""
+              }`}
+              onClick={() => handleTagClick(key)}
+            >
+              {key}({value})
+            </div>
+          );
+        })}
+        {selectedTags.length > 0 && (
+        <button
+          className="duration-300 flex items-center justify-center grow px-4 py-2 bg-red-300 text-white rounded-lg hover:bg-red-500"
+          onClick={() => setSelectedTags([])}
+        >
+          Clear
+        </button>
+      )}
+      </div>
+      
+    </>
   );
 }
