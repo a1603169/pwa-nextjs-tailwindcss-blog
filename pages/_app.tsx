d38eb05feb86f3cac434c14c9bb424daeae9825b
seunghunbang <upi21x@gmail.com>
2024-01-11 16:15:51 +0900

    Delete Ganalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,49 +1,18 @@
 import "@/styles/reset.css";
-import * as gtag from "../lib/gtag";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+// import * as gtag from "../lib/gtag";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+// Deprecated
 import type { AppProps } from "next/app";
 import WebLayout from "@/components/WebLayout";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-
   return (
     <>
       <Head>
         <title>NOT JUST A DEVELOPER</title>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        ></script>
-        <script
-          id="gtag-init"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
         <meta
           name="description"
           content="Welcome to my world. I am a Software Engineer who always find something funny!"
