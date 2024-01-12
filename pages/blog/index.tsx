63f31d4fc0f6ff410a6a7316abbdbed775b1da5f
seunghunbang <upi21x@gmail.com>
2024-01-12 15:53:13 +0900

    Update blog page effect

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -31,7 +31,6 @@ export default function Blog({
   for (let i = 1; i <= Math.ceil(filteredPosts.length / postsPerPage); i++) {
     pageNumbers.push(i);
   }
-  
   return (
     <Transition>
       <section className="flex flex-col items-center text-2xl text-indigo-300 pb-20">
@@ -40,21 +39,21 @@ export default function Blog({
           selectedTags={selectedTags}
           setSelectedTags={setSelectedTags}
         />
-        <ul className="flex w-3/4 flex-col gap-5 border-2 border-indigo-300 border-solid p-5 rounded-xl max-md:text-sm">
+        <ul className="flex w-3/4 flex-col gap-3 border-2 border-indigo-300 border-solid p-5 rounded-xl max-md:text-sm">
           {currentPosts.map(({ id, date, title, subtitle }) => {
             return (
-              <li className="hover:shadow-xl shadow-indigo-500/50" key={id}>
+              <li className="group shadow-indigo-500/50" key={id}>
                 <Link
-                  className="flex flex-col transition-all duration-300 border-indigo-300 border-b-2 border-solid hover:bg-indigo-300 hover:text-indigo-50 hover:rounded-t-lg hover:pt-2 hover:pb-1"
+                  className="flex flex-col border-indigo-300 border-b-2 border-solid pt-2 pb-1"
                   href={`/blog/${id}`}
                 >
-                  <div className="flex justify-between items-center">
+                  <div className="duration-300 flex justify-between items-center">
                     <h2 className="mx-2 truncate">{title}</h2>
                     <p className="text-sm mx-2 whitespace-nowrap max-w-1/2">
                       {date}
                     </p>
                   </div>
-                  <p className="text-xs mx-2 max-w-3/4 truncate">{subtitle}</p>
+                  <p className="text-xs mx-2 max-w-3/4 truncate opacity-0 group-hover:opacity-100 group-active:opacity-100 transition-opacity duration-500">{subtitle}</p>
                 </Link>
               </li>
             );
