45fd246dc2c2ad01e0619ad3d99ef659fa5998e7
seunghunbang <upi21x@gmail.com>
2024-03-05 01:32:03 +0900

    Fix web app status notch bar by setting it back to default

--- components/PersonalHead.tsx
+++ components/PersonalHead.tsx
@@ -4,11 +4,10 @@ export default function PersonalHead() {
   return (
     <>
       <meta name="viewport" content="initial-scale=1, viewport-fit=cover, width=device-width"></meta>
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
         <meta name="apple-mobile-web-app-title" content="Seunghun's Dev Log" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="msapplication-TileColor" content="#b0aceb" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
