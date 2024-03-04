3570ff61f3729e59a44dec72f4c8a76cbeb47e56
seunghunbang <upi21x@gmail.com>
2024-03-04 22:49:12 +0900

    Update the head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,12 +16,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>NOT JUST A DEVELOPER</title>
         <link rel="manifest" href="/manifest.json" />
         <meta name="viewport" content="initial-scale=1, viewport-fit=cover, width=device-width"></meta>
-        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
         <meta
           name="description"
           content="Welcome to my world. I am a Software Engineer who always find something funny!"
         />
+        <meta name="apple-mobile-web-app-title" content="Seunghun's Dev Log" />
+        <meta name="description" content="Communicate with Junior Dev who wants to grow themselves" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <WebLayout>
