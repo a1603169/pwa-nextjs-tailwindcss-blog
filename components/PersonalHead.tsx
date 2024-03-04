34bf6e2a74522221846746355b3fa948340b4942
seunghunbang <upi21x@gmail.com>
2024-03-05 01:39:11 +0900

    Fix bar style to black

--- components/PersonalHead.tsx
+++ components/PersonalHead.tsx
@@ -7,7 +7,7 @@ export default function PersonalHead() {
         <meta name="apple-mobile-web-app-title" content="Seunghun's Dev Log" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="msapplication-TileColor" content="#b0aceb" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
