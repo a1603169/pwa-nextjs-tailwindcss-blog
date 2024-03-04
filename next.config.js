e75e8788cfdc63f5e16e96c0eb74f965a84d37b9
seunghunbang <upi21x@gmail.com>
2024-03-04 22:14:08 +0900

    Config setting for pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+
+})
+
 const nextConfig = {
   reactStrictMode: false,
   env: {
@@ -6,4 +14,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
