e6d36538ccd56b7fa02aeb77e4fc640ad19c5ada
seunghunbang <upi21x@gmail.com>
2024-03-22 17:58:00 +0900

    Add marquee on the dev tags

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -20,25 +20,24 @@ export default function Blog({
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 10;
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
-  
+
   // Check if the post has any of the selected tags
   // If nothing is selected, show all posts
   // Previously, I checked if the post has all the selected tags so it was not working as expected
   let filteredPosts;
-  if(selectedTags.length === 0) {
+  if (selectedTags.length === 0) {
     filteredPosts = allPostsData;
-  } 
-  else {
+  } else {
     filteredPosts = allPostsData.filter((post) =>
       selectedTags.some((tag) => post.tags && post.tags.includes(tag))
     );
   }
-  
+
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
   const pageNumbers = [];
-  
+
   for (let i = 1; i <= Math.ceil(filteredPosts.length / postsPerPage); i++) {
     pageNumbers.push(i);
   }
@@ -50,33 +49,43 @@ export default function Blog({
           selectedTags={selectedTags}
           setSelectedTags={setSelectedTags}
         />
-        <ul id="blogContents" className="flex w-3/4 flex-col gap-5 border-2 border-indigo-300 border-solid p-5 rounded-xl max-md:text-sm">
+        <ul
+          id="blogContents"
+          className="flex w-3/4 flex-col gap-5 border-2 border-indigo-300 border-solid p-5 rounded-xl max-md:text-sm"
+        >
           {currentPosts.map(({ id, date, title, subtitle, tags }) => {
-            console.log(tags)
+            console.log(tags);
             return (
               <li className="group relative shadow-indigo-500/50" key={id}>
                 <Link
                   className="flex flex-col border-indigo-300 border-b-2 border-solid pt-2 pb-1"
                   href={`/blog/${id}`}
                 >
-                  <div className="duration-300 flex justify-between items-center">
-                    <h2 className="mx-2 truncate">
-                      {title}
-                    </h2>
+                  <div className="duration-300 flex justify-between items-center overflow-x-hidden">
+                    <div className="overflow-x-hidden">
+                      <h2 className="mx-2">{title}</h2>
+                    </div>
                     <p className="text-sm mx-2 whitespace-nowrap max-w-1/2">
                       {date}
                     </p>
                   </div>
-                  <p className="text-sm mx-2 my-1 mb-2 whitespace-nowrap">
-                    {tags.map((tag, idx) => {
-                      return (
-                        <span key={idx} className="bg-indigo-300 text-indigo-50 px-2 py-1 rounded-lg mr-1">
-                          {tag}
-                        </span>
-                      );
-                    })}
+                  <div className="overflow-x-hidden">
+                    <p className="text-sm mx-2 my-1 mb-2 whitespace-nowrap hover:max-sm:animate-marquee">
+                      {tags.map((tag, idx) => {
+                        return (
+                            <span
+                              key={idx}
+                              className="bg-indigo-300 text-indigo-50 px-2 py-1 rounded-lg mr-1"
+                            >
+                              {tag}
+                            </span>
+                        );
+                      })}
+                    </p>
+                  </div>
+                  <p className="text-xs mx-2 max-w-3/4 truncate opacity-0 group-hover:opacity-100 group-active:opacity-100 transition-opacity duration-500 max-xl:opacity-100">
+                    {subtitle}
                   </p>
-                  <p className="text-xs mx-2 max-w-3/4 truncate opacity-0 group-hover:opacity-100 group-active:opacity-100 transition-opacity duration-500 max-xl:opacity-100">{subtitle}</p>
                 </Link>
               </li>
             );
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,20 @@ module.exports = {
   theme: {
     fontFamily: {},
     extend: {
+      animation: {
+        marquee: "marquee 3s linear infinite",
+        marquee2: "marquee2 10s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
       maxHeight: {
         '128': '32rem'
       },
