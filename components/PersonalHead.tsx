663090a7bf5b6aa59d35307858ebe2136979e39b
seunghunbang <upi21x@gmail.com>
2024-03-25 15:53:02 +0900

    Fix pwa styling notch color

--- components/PersonalHead.tsx
+++ components/PersonalHead.tsx
@@ -6,7 +6,7 @@ export default function PersonalHead() {
         <meta name="apple-mobile-web-app-title" content="Seunghun's Dev Log" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="white" />
         <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
         <meta name="msapplication-TileColor" content="#fff" />
         <meta name="msapplication-tap-highlight" content="no" />
