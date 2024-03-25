7543bdb8bf0c76f9ead2c866834e34ade919ab8a
seunghunbang <upi21x@gmail.com>
2024-03-25 15:41:51 +0900

    Fix pwa ios notch area we have to download it through safari

--- components/PersonalHead.tsx
+++ components/PersonalHead.tsx
@@ -3,12 +3,12 @@ import Head from "next/head";
 export default function PersonalHead() {
   return (
     <>
-      <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-title" content="Seunghun's Dev Log" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="msapplication-TileColor" content="#b0aceb" />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
+        <meta name="msapplication-TileColor" content="#7986cb" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
         <link rel="shortcut icon" href="/favicon.ico" />
