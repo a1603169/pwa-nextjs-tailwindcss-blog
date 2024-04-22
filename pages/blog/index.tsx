731675213ab33128f93b4dc543de7142c22de1ff
seunghunbang <upi21x@gmail.com>
2024-04-22 17:25:29 +0900

    initialize pr for search engine

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import { getSortedPostsData, getAllTags } from "@/lib/post";
 import { useState } from "react";
 import Transition from "@/components/Transition";
 import Filters from "@/components/Filters";
+import { getSortedPostsData, getAllTags } from "@/lib/post";
 
 export default function Blog({
   allPostsData,
@@ -13,86 +13,97 @@ export default function Blog({
     title: string;
     subtitle: string;
     id: string;
-    tags: string[]; // Add the 'tags' property to the type definition
+    tags: string[];
+    content?: string;
   }[];
   tags: object;
 }) {
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 10;
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  // Check if the post has any of the selected tags
-  // If nothing is selected, show all posts
-  // Previously, I checked if the post has all the selected tags so it was not working as expected
-  let filteredPosts;
-  if (selectedTags.length === 0) {
-    filteredPosts = allPostsData;
-  } else {
-    filteredPosts = allPostsData.filter((post) =>
-      selectedTags.some((tag) => post.tags && post.tags.includes(tag))
+  // 유효한 문자열로 변환
+  const normalizeString = (str: string) => str.toLowerCase().trim();
+
+  let filteredPosts = allPostsData.filter((post) => {
+    const title = normalizeString(post.title || "");
+    const subtitle = normalizeString(post.subtitle || "");
+    const content = normalizeString(post.content || "");
+    const search = normalizeString(searchTerm);
+
+    return (
+      (selectedTags.length === 0 || selectedTags.some((tag) => post.tags?.includes(tag))) &&
+      (search === "" || title.includes(search) || subtitle.includes(search) || content.includes(search))
     );
-  }
+  });
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
-  const pageNumbers = [];
 
+  const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(filteredPosts.length / postsPerPage); i++) {
     pageNumbers.push(i);
   }
+
   return (
     <Transition>
       <section className="flex flex-col items-center text-2xl text-indigo-300 pb-20">
+        <h3>검색 키워드</h3>
+        <input
+          type="text"
+          placeholder="검색..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="mb-4 border-2 border-indigo-300 border-solid p-2 w-3/4 rounded-lg max-md:text-sm"
+        />
+        
         <Filters
           tags={tags}
           selectedTags={selectedTags}
           setSelectedTags={setSelectedTags}
-          // When Filter component is clicked, the page should go 1.
           setCurrentPage={setCurrentPage}
         />
-        {/* To direct the right position of the app */}
-        <div id="blogContents" className="absolute"></div>
+
         <ul className="flex w-3/4 flex-col gap-5 border-2 border-indigo-300 border-solid p-5 rounded-xl max-md:text-sm">
-          {currentPosts.map(({ id, date, title, subtitle, tags }) => {
-            return (
-              <li className="group relative shadow-indigo-500/50" key={id}>
-                <Link
-                  className="flex flex-col border-indigo-300 border-b-2 border-solid pt-2 pb-1"
-                  href={`/blog/${id}`}
-                >
-                  <div className="duration-300 flex justify-between items-center overflow-x-hidden">
-                    <div className="overflow-x-hidden">
-                      <h2 className="mx-2">{title}</h2>
-                    </div>
-                    <p className="text-sm mx-2 whitespace-nowrap max-w-1/2">
-                      {date}
-                    </p>
+          {currentPosts.map(({ id, date, title, subtitle, tags }) => (
+            <li className="group relative shadow-indigo-500/50" key={id}>
+              <Link
+                className="flex flex-col border-indigo-300 border-b-2 border-solid pt-2 pb-1"
+                href={`/blog/${id}`}
+              >
+                <div className="duration-300 flex justify-between items-center overflow-x-hidden">
+                  <div className="overflow-x-hidden">
+                    <h2 className="mx-2">{title}</h2>
                   </div>
+                  <p className="text-sm mx-2 whitespace-nowrap max-w-1/2">
+                    {date}
+                  </p>
+                </div>
+
                   <div className="overflow-x-hidden">
                     <p className="text-sm mx-2 my-1 mb-2 whitespace-nowrap hover:max-sm:animate-marquee">
-                      {tags.map((tag, idx) => {
-                        return (
-                          <span
-                            key={idx}
-                            className="bg-indigo-300 text-indigo-50 px-2 py-1 rounded-lg mr-1 max-sm:text-xs"
-                          >
-                            {tag}
-                          </span>
-                        );
-                      })}
+                      {tags.map((tag, idx) => (
+                        <span
+                          key={idx}
+                          className="bg-indigo-300 text-indigo-50 px-2 py-1 rounded-lg mr-1 max-sm:text-xs"
+                        >
+                          {tag}
+                        </span>
+                      ))}
                     </p>
                   </div>
+                  
                   <p className="text-xs mx-2 max-w-3/4 truncate opacity-0 group-hover:opacity-100 group-active:opacity-100 transition-opacity duration-500 max-xl:opacity-100">
                     {subtitle}
                   </p>
-                </Link>
-              </li>
-            );
-          })}
+              </Link>
+            </li>
+          ))}
         </ul>
+        
         <div className="flex justify-center w-3/4 mt-10 gap-2">
-          {currentPage > 6 && <span>...</span>}
           {pageNumbers
             .slice(Math.max(0, currentPage - 5), currentPage + 5)
             .map((number) => (
